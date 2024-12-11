a3565cb5f797fa0f8f3bec2dc45f663812406cf5
rbv3 <drigovalenca@gmail.com>
2024-12-10 21:37:50 -0300

    update remoteObject to use proxy

--- client/appRemoteObject.js
+++ client/appRemoteObject.js
@@ -1,4 +1,36 @@
 const { pollRemoteObject } = require('./pollObject/pollRemoteObject');
+const proxy = require('./proxy/remoteProxy');
 
-// Poll Object genérico para monitorar a operação remota
-pollRemoteObject.start();
+class RemoteObject {
+    constructor() {
+        this.isComplete = false;
+    }
+    async fetchResult(taskId) {
+        try {
+            const { status, result } = await proxy.operation.getStatus(taskId);
+            this.isComplete = status == 'completed';
+
+            if (this.isComplete) {
+                console.log('Result:', result);
+            }
+
+            return this.isComplete;
+        } catch (error) {
+            console.error('Error during polling:', error.message);
+            return false;
+        }
+    }
+};
+(async () => {
+    remoteObject = new RemoteObject();
+    try {
+        // Inicia a operação remota usando o remoteProxy
+        const taskId = await proxy.operation.invoke('operation', 'computeTask', [5, 10]);
+        console.log('Task ID:', taskId);
+
+        // Inicia o polling com o remoteProxy para verificar o status
+        pollRemoteObject(remoteObject, taskId).start();
+    } catch (error) {
+        console.error('Error starting operation:', error.message);
+    }
+})();
